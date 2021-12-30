ebc0547fe5ec2332d6f13ee00687f8028497d70f
UIT21521645 <96382362+UIT21521645@users.noreply.github.com>
2021-12-30 15:55:22 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -31,3 +31,16 @@ int main()
 {
     return 0;
 }
+void tao_qua()//Tạo quả nằm ở vị trí random
+{
+	do
+	{
+		xqua = rand() % (109 - 11 + 1) + 11;
+		yqua = rand() % (27 - 8 + 1) + 8;
+	} while (ktra_ran_trung_qua());
+}
+void ve_qua()//Vẽ quả
+{
+	gotoXY(xqua, yqua);
+	cout << "O";
+}
